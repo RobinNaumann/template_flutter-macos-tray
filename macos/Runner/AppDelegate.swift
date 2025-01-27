7d8308316c4f990ef6d42512f11484804a4081e1
Robin Naumann <constorux@gmail.com>
2025-01-27 10:25:08 +0100

    feat: prevent multiple instances

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+
 //import ServiceManagement
 
 @main
@@ -7,7 +8,9 @@ class AppDelegate: FlutterAppDelegate {
   var statusBar: StatusBarController?
   var popover = NSPopover.init()
   override init() {
-    popover.behavior = NSPopover.Behavior.transient //to make the popover hide when the user clicks outside of it
+    popover.behavior = NSPopover.Behavior.transient  //to make the popover hide when the user clicks outside of it
+    // quit if another instance is already running.
+    AppDelegate.alreadyRunningGuard()
   }
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return false
@@ -15,19 +18,40 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
+
   override func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // uncomment the line below and the import statement 
+    // uncomment the line below and the import statement
     // at the top to enable auto-launching on login
     //do {try SMAppService.mainApp.register()} catch {}
-    
-    let controller: FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
-    popover.contentViewController = controller 
+
+    let controller: FlutterViewController =
+      mainFlutterWindow?.contentViewController as! FlutterViewController
+    popover.contentViewController = controller
     popover.contentSize = NSSize(width: 360, height: 250)
-    
+
     statusBar = StatusBarController.init(popover)
     mainFlutterWindow?.close()
-    
+
     super.applicationDidFinishLaunching(aNotification)
-    
+
+  }
+
+  // if the app is already running, show an alert and terminate the new instance.
+  // this is to prevent multiple instances of a tray app running at the same time.
+  private static func alreadyRunningGuard() {
+    let bundleID = Bundle.main.bundleIdentifier!
+    let runCount = NSRunningApplication.runningApplications(withBundleIdentifier: bundleID).count
+
+    if runCount <= 1 {
+      return
+    }
+
+    let a = NSAlert()
+    a.messageText = "App already running"
+    a.informativeText = "Another instance of this app is already running. Please close it first."
+    a.addButton(withTitle: "OK")
+    a.runModal()
+    NSApp.terminate(nil)
+
   }
 }
